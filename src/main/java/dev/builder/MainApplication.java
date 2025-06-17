f915928cc077eea8730448df4a61ed8037638a65
Emmanuel <20243ds155@utez.edu.mx>
2025-06-17 14:35:29 -0600

    fix: logo-imgage

--- src/main/java/dev/builder/MainApplication.java
+++ src/main/java/dev/builder/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Kedu");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
